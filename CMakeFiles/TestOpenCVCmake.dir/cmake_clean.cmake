480b1bdfdc1690ee42f3450cafa66a093db8d7b4
pi <zinan1996@gmail.com>
2019-11-27 04:36:36 +0700

    ok

---
+++ CMakeFiles/TestOpenCVCmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestOpenCVCmake.dir/main.cpp.o"
+  "TestOpenCVCmake.pdb"
+  "TestOpenCVCmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestOpenCVCmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
